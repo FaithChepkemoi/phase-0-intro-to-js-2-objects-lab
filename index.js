d2e7a54835ace0ad8bb2c23c3da22da44fbb3674
FaithChepkemoi <faithhope017@gmail.com>
2024-12-12 13:13:28 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+                     name: "Faith Nyolei" ,
+                     streetAdress: "605 Nyeri"
+                     } ;
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value };
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+      employee[key]= value;
+      return employee;
+
+}
+function deleteFromEmployeeByKey(employee, key)
+{
+     const { [key]: omitted, ...newEmployee} = employee;
+     return newEmployee;
+  
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; // Directly delete the property from the original object
+    return employee; // Return the mutated object
+}
